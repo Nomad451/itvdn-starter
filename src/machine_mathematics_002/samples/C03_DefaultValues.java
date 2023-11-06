8cb45e02a8f9d2c6cf97acae8c3e57c21a699703
Nomad <yakushev_v@rambler.ru>
2023-11-06 13:37:58 +0300

    add lesson_02

---
+++ src/machine_mathematics_002/samples/C03_DefaultValues.java
@@ -0,0 +1,16 @@
+package machine_mathematics_002.samples;
+
+public class C03_DefaultValues {
+    public static void main(String[] args) {
+        // На 7-й строке, создаем переменную с именем a, типа byte и не присваиваем ей никакого значения.
+
+        byte a;
+        boolean b;
+        char c;
+        String s;
+
+        // На 14-й строке - ОШИБКА: Запрещается использование неинициализированной локальной переменной!
+
+        //System.out.println(a);
+    }
+}
