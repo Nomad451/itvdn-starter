de36e3d10c74ef79770d38a5a17a2582858cae33
Nomad <yakushev_v@rambler.ru>
2023-11-06 14:11:49 +0300

    completed homework

---
+++ src/machine_mathematics_002/homework/AdditionalTask.java
@@ -0,0 +1,17 @@
+package machine_mathematics_002.homework;
+
+public class AdditionalTask {
+    public static void main(String[] args) {
+        char a1 = 'В';
+        char a2 = 'и';
+        char a3 = 'т';
+        char a4 = 'а';
+        char a5 = 'л';
+        char a6 = 'и';
+        char a7 = 'й';
+
+        System.out.println("Здравствуйте, " + a1 + a2 + a3 + a4 + a5 + a6 + a7 + " !");
+    }
+}
+
+
---
+++ src/machine_mathematics_002/homework/Task_2.java
@@ -0,0 +1,16 @@
+package machine_mathematics_002.homework;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        // Первый вариант
+//        double pi = Math.PI;
+//        double e = Math.E;
+
+        // Второй вариант
+        double pi = 3.141592653;
+        double e = 2.7182818284590452;
+
+        System.out.println("PI: " + pi);
+        System.out.println("E: " + e);
+    }
+}
