4011387043b5c94e70bbaad59e71de8213254dc9
Nomad <yakushev_v@rambler.ru>
2023-12-11 12:53:00 +0300

    completed homework

---
+++ src/conditions_004/homework/Interval.java
@@ -0,0 +1,26 @@
+package conditions_004.homework;
+
+import java.util.Scanner;
+
+public class Interval {
+    public static void main(String[] args) {
+        System.out.println("Введите число от 0 до 100:");
+
+        // Вводим число с клавиатуры
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+
+        // Проверяем наше число в заданном диапазоне
+        if (num >= 0 && num <= 14) {
+            System.out.println("Вошел в промежуток [0 - 14]");
+        } else if (num >= 15 && num <= 35) {
+            System.out.println("Вошел в промежуток [15 - 35]");
+        } else if (num >= 36 && num <= 49) {
+            System.out.println("Вошел в промежуток [36 - 49]");
+        } else if (num >= 50 && num <= 100) {
+            System.out.println("Вошел в промежуток [50 - 100]");
+        } else {
+            System.out.println("Вы не вошли в промежуток, попробуйте снова");
+        }
+    }
+}
