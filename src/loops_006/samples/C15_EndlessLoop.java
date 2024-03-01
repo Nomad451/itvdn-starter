88e3871a85407913df84439b167650f311fa7b01
Nomad <yakushev_v@rambler.ru>
2024-03-01 14:55:35 +0300

    add lesson_006

---
+++ src/loops_006/samples/C15_EndlessLoop.java
@@ -0,0 +1,23 @@
+package loops_006.samples;
+
+public class C15_EndlessLoop {
+    public static void main(String[] args) {
+        // 1.
+        while (true) {
+
+        }
+
+        // 2.
+        /*
+        do {
+
+        }
+        while (true);
+
+        // 3.
+        for ( ; ; ) {
+
+        }
+        */
+    }
+}
