b4eaa4205b01ce3f9970569b03305e6bea211e8c
Nomad <yakushev_v@rambler.ru>
2024-03-02 16:43:17 +0300

    completed homework

---
+++ src/arrays_009/homework/Arrays.java
@@ -0,0 +1,16 @@
+package arrays_009.homework;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        // Заполняем массив 10 элементами
+
+        int[] a = {5, 3, 33, 7, 0, -12, -17, 28, 159, 11};
+
+        // Выводим в обратном порядке
+
+        for (int i = a.length - 1; i >= 0; i--) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
